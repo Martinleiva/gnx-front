2c0ecf517fc6f97e7a51e01af20098c0d0069310
martin <martin_leiva13@hotmail.com>
2020-07-01 10:01:37 -0300

    Agregado de Mutation nuevo departamento

--- src/components/Departamentos.js
+++ src/components/Departamentos.js
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
-const QUERY = gql`
-query {
-    departments{
-      id
-      dept_name
+const LISTADO_DPTOS = gql`
+    query {
+        departments{
+        id
+        dept_name
+        }
     }
-  }
+`;
+
+const NUEVO_DPTO = gql`
+    mutation adddepartment($input: DepartmentTypeInput!) {
+        adddepartment(input: $input) {
+            id
+            dept_name
+        }
+    }
 `;
 
 const Departamentos = () => {
 
     //Obtener dptos de GraphQL
-    const { data, loading, error } = useQuery(QUERY);
+    const { data, loading } = useQuery(LISTADO_DPTOS);
+    const [ adddepartment ] = useMutation(NUEVO_DPTO, {
+        update(cache, { data: adddepartment }) {
+            //Obtener el objeto de cache que queremos actualizar
+            const { departments } = cache.readQuery({ query: LISTADO_DPTOS});
+
+            // Reescribimos el cache (el cache nunca se debe modificar)
+            cache.writeQuery({
+                query: LISTADO_DPTOS,
+                data: {
+                    departments: [...departments, adddepartment]
+                }
+            });
+        }
+    });
 
     const [departamento, guardarDepartamento] = useState(false);
+    const [agregado, guardarAgregado] = useState(false);
 
     const handleClick = () => {
         guardarDepartamento(true);
@@ -26,16 +50,37 @@ const Departamentos = () => {
     //Validar Form
     const formik = useFormik({
         initialValues: {
-            id: 3,
             nuevodpto: ''
         },
         validationSchema: Yup.object({
             nuevodpto: Yup.string().required('El nombre del departamento es obligatorio')
         }),
-        onSubmit: valores => {
-            console.log('enviando...');
-            valores.id = valores.id + 1;
-            console.log(valores);
+        onSubmit: async valores => {
+
+            const { nuevodpto } = valores;
+
+            try {
+                const { data } = await adddepartment({
+                    variables: {
+                        input: {
+                            dept_name: nuevodpto
+                        }
+                    }
+                });
+
+                guardarAgregado(true);
+                
+                console.log(data);
+                setTimeout(() => {
+                    guardarAgregado(false);
+                    guardarDepartamento(false);
+                    formik.values.nuevodpto = '';
+                }, 2000);
+                
+
+            } catch (error) {
+                console.log(error);
+            }
         }
     });
 
@@ -49,17 +94,17 @@ const Departamentos = () => {
                     <table className="table-auto">
                         <thead className="bg-yellow-600">
                             <tr>
-                                <th className="px-4 py-2 text-white">ID</th>
+                                <th className="px-4 py-2 text-white">Nro</th>
                                 <th className="px-4 py-2 text-white">Nombre</th>
                                 <th className="px-4 py-2 text-white">Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
                             
-                            {data.departments .map( dpto => (
+                            {data.departments.map( (dpto,index) => (
                                 <tr key={dpto.id}>
                                     <td className="border px-4 py-2">
-                                        {dpto.id}
+                                        {index + 1}
                                     </td>
                                     <td className="border px-4 py-2">
                                         {dpto.dept_name}
@@ -120,12 +165,21 @@ const Departamentos = () => {
                                 />
                             </div>
 
-                            { formik.touched.nuevodpto && formik.errors.nuevodpto ? (
+                            { formik.touched.nuevodpto && formik.errors.nuevodpto  ? (
                                 <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2">
                                     <p className="font-bold">Error</p>
                                     <p>{formik.errors.nuevodpto}</p>
                                 </div>
-                            ) : null }
+                            ) : null
+                            }
+
+                            { agregado ? 
+                                    (
+                                        <div className="my-2 bg-red-100 border-l-4 border-green-500 text-green-700 p-2">
+                                            <p className="font-bold">Agregado!</p>
+                                        </div>
+                                ) : null
+                            }
 
                         </form>
                     </div>
