1889e4d09d149a9a128651af7e4dec432d0bcec0
estenoesmonti <estenoesmonti@gmail.com>
2020-07-06 10:29:12 -0300

    cambios en json de salarios y agregar query de add salary 2

--- src/components/Salarios.js
+++ src/components/Salarios.js
@@ -1,14 +1,36 @@
 // 3 - Salaries must have empId, salary, from_date, to_date
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import React, { useState } from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client';
+import { useFormik } from 'formik';
+import { Formik } from 'formik';
+
 
 const SALARY_LIST = gql`
   query {
-    salaries {
+    salaries{
+      id
+      salary
+      from_date
+      to_date
+      employee {
+        id
+        dni
+        first_name
+        last_name
+      }
+    }
+  }
+`;
+
+const SALARY_ADD = gql`
+  mutation addsalary($input: SalaryTypeInput!) {
+    addsalary(input: $input){
       id
       employee {
         id
         dni
+        first_name
+        last_name
       }
       salary
       from_date
@@ -19,12 +41,32 @@ const SALARY_LIST = gql`
 
 const Salaries = () => {
 
+  // obtener datos del query
   const { data, loading } = useQuery(SALARY_LIST);
+  // console.log(data);
 
-  console.log(data);
+  // Mutation para añadir un Salary
+  const [ addSalary ] = useMutation( SALARY_ADD, {
+    
+    update(cache, { data: addSalary }) {
+      
+      // Obtener el obj de cache que queremos actualizar
+      const { salaries } = cache.readQuery({ query: SALARY_LIST });
 
-  // const [salaries, setSalaries] = useState({});
-  // const [error, setError] = useState(false);
+      // Reescribimos el cache
+      cache.writeQuery({
+        query: SALARY_LIST,
+        data: {
+          salaries: [ ...salaries, addSalary ]
+        }
+      });
+    }
+  });
+
+  const [ salaries, setSalaries ] = useState({});
+  const [ error, setError ] = useState(false);
+  const [ message, setMessage ] = useState(null);
+  const [ added, setAdded ] = useState(false);
 
   // const handleChange = (event) => {
   //   setSalaries({
@@ -37,7 +79,7 @@ const Salaries = () => {
   // useEffect( () => {
 
   // })
-  
+
 
   // There aren't Salaries
   //const notSalaries = 0;
@@ -49,35 +91,35 @@ const Salaries = () => {
       <h1 className="py-5 pl-8 text-center"><u>Listado de Salarios</u></h1>
       <div className="m-5 flex justify-center">
         {!data.salaries ? "No cargaste nada wn xd" : (
-        <table className="table-auto w-full">
-          <thead className="bg-yellow-600">
-            <tr>
-              <th className="px-4 py-2 text-white">Nro</th>
-              <th className="px-4 py-2 text-white">DNI</th>
-              <th className="px-4 py-2 text-white">Salario</th>
-              <th className="px-4 py-2 text-white">Fecha Desde</th>
-              <th className="px-4 py-2 text-white">Fecha Hasta</th>
-              <th className="px-4 py-2 text-white">Acciones</th>
+          <table className="table-auto w-full">
+            <thead className="bg-yellow-600">
+              <tr>
+                <th className="px-4 py-2 text-white">Nro</th>
+                <th className="px-4 py-2 text-white">DNI</th>
+                <th className="px-4 py-2 text-white">Salario</th>
+                <th className="px-4 py-2 text-white">Fecha Desde</th>
+                <th className="px-4 py-2 text-white">Fecha Hasta</th>
+                <th className="px-4 py-2 text-white">Acciones</th>
 
-            </tr>
-          </thead>
-          <tbody>
-            {data.salaries.map( (salary, index) => (
-              <tr className="text-center" key={salary.id}>
-                <td className="border px-4 py-2">{index + 1}</td>
-                <td className="border px-4 py-2">36235123</td>
-                <td className="border px-4 py-2">{salary.salary}</td>
-                <td className="border px-4 py-2">{salary.from_date}</td>
-                <td className="border px-4 py-2">{salary.to_date}</td>
-                <td className="border px-4 py-2">
-                  <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-6 mr-6 shadow-md">Modificar</button>
-                  <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded m-1 shadow-md">Eliminar</button>
-                </td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            </thead>
+            <tbody>
+              {data.salaries.map((salary, index) => (
+                <tr className="text-center" key={salary.id}>
+                  <td className="border px-4 py-2">{index + 1}</td>
+                  <td className="border px-4 py-2">{salary.dni}</td>
+                  <td className="border px-4 py-2">{salary.salary}</td>
+                  <td className="border px-4 py-2">{salary.from_date}</td>
+                  <td className="border px-4 py-2">{salary.to_date}</td>
+                  <td className="border px-4 py-2">
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-6 mr-6 shadow-md">Modificar</button>
+                    <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded m-1 shadow-md">Eliminar</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
